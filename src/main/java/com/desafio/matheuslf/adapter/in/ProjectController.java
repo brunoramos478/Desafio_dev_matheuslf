db7d1598b0e2186e71e723914f07dd65aac9bd9c
brunoramos478 <brunoramoss296@gmail.com>
2026-06-11 20:23:23 -0300

    update: Novos métodos no conthroller, listagem e criação de projetos.

--- src/main/java/com/desafio/matheuslf/adapter/in/ProjectController.java
+++ src/main/java/com/desafio/matheuslf/adapter/in/ProjectController.java
@@ -1,11 +1,34 @@
 package com.desafio.matheuslf.adapter.in;
 
+import com.desafio.matheuslf.application.service.ApplicationService;
+import com.desafio.matheuslf.shared.dto.ProjectDto;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
 @AllArgsConstructor
 public class ProjectController {
+
+    private final ApplicationService service;
+
+    @ApiResponse(responseCode = "201", description = "Created")
+    @PostMapping()
+    public ResponseEntity<ProjectDto> newProject(@Valid @RequestBody ProjectDto dto) {
+        var response = service.createProject(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @ApiResponse(responseCode = "200", description = "OK")
+    @GetMapping()
+    public ResponseEntity<List<ProjectDto>> listAllProject(Pageable pageable) {
+        var response = service.listAllProjects(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(response.toList());
+    }
 }
